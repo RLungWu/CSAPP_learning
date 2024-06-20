b74724bd700a5ab0c217de0aa2f4daac2ae918ab
RLungWu <larry14789@gmail.com>
2024-06-20 18:28:21 +0800

    Finish Phase 123

--- 2-bomb/bomb.asm
+++ 2-bomb/bomb.asm
@@ -262,8 +262,8 @@ Disassembly of section .text:
   400d9f:	90                   	nop
 
 0000000000400da0 <main>:
-  400da0:	53                   	push   %rbx
-  400da1:	83 ff 01             	cmp    $0x1,%edi
+  400da0:	53                   	push   %rbx                       
+  400da1:	83 ff 01             	cmp    $0x1,%edi                  # compare the number of arguments
   400da4:	75 10                	jne    400db6 <main+0x16>
   400da6:	48 8b 05 9b 29 20 00 	mov    0x20299b(%rip),%rax        # 603748 <stdin@@GLIBC_2.2.5>
   400dad:	48 89 05 b4 29 20 00 	mov    %rax,0x2029b4(%rip)        # 603768 <infile>
@@ -344,10 +344,10 @@ Disassembly of section .text:
   400edf:	90                   	nop
 
 0000000000400ee0 <phase_1>:
-  400ee0:	48 83 ec 08          	sub    $0x8,%rsp
-  400ee4:	be 00 24 40 00       	mov    $0x402400,%esi
-  400ee9:	e8 4a 04 00 00       	callq  401338 <strings_not_equal>
-  400eee:	85 c0                	test   %eax,%eax
+  400ee0:	48 83 ec 08          	sub    $0x8,%rsp                    # allocate 8 bytes on the stack
+  400ee4:	be 00 24 40 00       	mov    $0x402400,%esi               # move the address of the string to esi
+  400ee9:	e8 4a 04 00 00       	callq  401338 <strings_not_equal>   # call the function strings_not_equal
+  400eee:	85 c0                	test   %eax,%eax                    # test the return value # eas is the return value
   400ef0:	74 05                	je     400ef7 <phase_1+0x17>
   400ef2:	e8 43 05 00 00       	callq  40143a <explode_bomb>
   400ef7:	48 83 c4 08          	add    $0x8,%rsp
@@ -356,62 +356,64 @@ Disassembly of section .text:
 0000000000400efc <phase_2>:
   400efc:	55                   	push   %rbp
   400efd:	53                   	push   %rbx
-  400efe:	48 83 ec 28          	sub    $0x28,%rsp
-  400f02:	48 89 e6             	mov    %rsp,%rsi
-  400f05:	e8 52 05 00 00       	callq  40145c <read_six_numbers>
-  400f0a:	83 3c 24 01          	cmpl   $0x1,(%rsp)
-  400f0e:	74 20                	je     400f30 <phase_2+0x34>
-  400f10:	e8 25 05 00 00       	callq  40143a <explode_bomb>
-  400f15:	eb 19                	jmp    400f30 <phase_2+0x34>
-  400f17:	8b 43 fc             	mov    -0x4(%rbx),%eax
-  400f1a:	01 c0                	add    %eax,%eax
-  400f1c:	39 03                	cmp    %eax,(%rbx)
+  400efe:	48 83 ec 28          	sub    $0x28,%rsp                 # 保留40个bits的空間 => why? 因為第一個會檢查是1，後續只需要檢查5個int，8 * 5 = 40
+  400f02:	48 89 e6             	mov    %rsp,%rsi                  # move rsp => rsi => 用來儲存讀取的數字並傳到 read_six_numbers當中
+  400f05:	e8 52 05 00 00       	callq  40145c <read_six_numbers>  # call read_six_numbers
+  400f0a:	83 3c 24 01          	cmpl   $0x1,(%rsp)                # compare the first number with 1 
+  400f0e:	74 20                	je     400f30 <phase_2+0x34>      # if the first number is 1, jump to 400f30
+  400f10:	e8 25 05 00 00       	callq  40143a <explode_bomb>      
+  400f15:	eb 19                	jmp    400f30 <phase_2+0x34>       
+                                                                  # 循環開始的地方
+  400f17:	8b 43 fc             	mov    -0x4(%rbx),%eax            # move the number to eax aka 獲取前一個數字
+  400f1a:	01 c0                	add    %eax,%eax                  # double it
+  400f1c:	39 03                	cmp    %eax,(%rbx)                # 將double 以後的數字與原來的數字比較
   400f1e:	74 05                	je     400f25 <phase_2+0x29>
   400f20:	e8 15 05 00 00       	callq  40143a <explode_bomb>
-  400f25:	48 83 c3 04          	add    $0x4,%rbx
-  400f29:	48 39 eb             	cmp    %rbp,%rbx
-  400f2c:	75 e9                	jne    400f17 <phase_2+0x1b>
+  400f25:	48 83 c3 04          	add    $0x4,%rbx                  # 將 rbs 移動到下一個整數
+  400f29:	48 39 eb             	cmp    %rbp,%rbx                  # 檢查是否已經檢查完5個整數 下面的循環。如果一樣的話，會執行下一行，否則會執行下下一行，跳出循環
+  400f2c:	75 e9                	jne    400f17 <phase_2+0x1b>      
   400f2e:	eb 0c                	jmp    400f3c <phase_2+0x40>
-  400f30:	48 8d 5c 24 04       	lea    0x4(%rsp),%rbx
-  400f35:	48 8d 6c 24 18       	lea    0x18(%rsp),%rbp
-  400f3a:	eb db                	jmp    400f17 <phase_2+0x1b>
-  400f3c:	48 83 c4 28          	add    $0x28,%rsp
-  400f40:	5b                   	pop    %rbx
+                                                                  # 以下是一個循環，檢查5個int是否符合條件
+  400f30:	48 8d 5c 24 04       	lea    0x4(%rsp),%rbx             # 設置 rbs 為指向第二個整數的地址  (一開始的 40 Bits 空間)
+  400f35:	48 8d 6c 24 18       	lea    0x18(%rsp),%rbp            # 設置 rbp 為指向最後一個整數的地址
+  400f3a:	eb db                	jmp    400f17 <phase_2+0x1b>      
+  400f3c:	48 83 c4 28          	add    $0x28,%rsp                 # 回收空間，與sub $0x28,%rsp對應
+  400f40:	5b                   	pop    %rbx             # 回收rbx   
   400f41:	5d                   	pop    %rbp
   400f42:	c3                   	retq   
 
 0000000000400f43 <phase_3>:
-  400f43:	48 83 ec 18          	sub    $0x18,%rsp
-  400f47:	48 8d 4c 24 0c       	lea    0xc(%rsp),%rcx
-  400f4c:	48 8d 54 24 08       	lea    0x8(%rsp),%rdx
-  400f51:	be cf 25 40 00       	mov    $0x4025cf,%esi
-  400f56:	b8 00 00 00 00       	mov    $0x0,%eax
-  400f5b:	e8 90 fc ff ff       	callq  400bf0 <__isoc99_sscanf@plt>
-  400f60:	83 f8 01             	cmp    $0x1,%eax
-  400f63:	7f 05                	jg     400f6a <phase_3+0x27>
+  400f43:	48 83 ec 18          	sub    $0x18,%rsp                     # allocate 24 bytes on the stack
+  400f47:	48 8d 4c 24 0c       	lea    0xc(%rsp),%rcx                 # move the address of the first number to rcx
+  400f4c:	48 8d 54 24 08       	lea    0x8(%rsp),%rdx                 # move the address of the second number to rdx
+  400f51:	be cf 25 40 00       	mov    $0x4025cf,%esi                 # 0x4025cf = "%d %d"
+  400f56:	b8 00 00 00 00       	mov    $0x0,%eax                      # set eax to 0
+  400f5b:	e8 90 fc ff ff       	callq  400bf0 <__isoc99_sscanf@plt>   # call sscanf
+  400f60:	83 f8 01             	cmp    $0x1,%eax                       # compare the return value with 1
+  400f63:	7f 05                	jg     400f6a <phase_3+0x27>           # if the return value is greater than 1, jump to 400f6a
   400f65:	e8 d0 04 00 00       	callq  40143a <explode_bomb>
-  400f6a:	83 7c 24 08 07       	cmpl   $0x7,0x8(%rsp)
-  400f6f:	77 3c                	ja     400fad <phase_3+0x6a>
-  400f71:	8b 44 24 08          	mov    0x8(%rsp),%eax
-  400f75:	ff 24 c5 70 24 40 00 	jmpq   *0x402470(,%rax,8)
-  400f7c:	b8 cf 00 00 00       	mov    $0xcf,%eax
-  400f81:	eb 3b                	jmp    400fbe <phase_3+0x7b>
-  400f83:	b8 c3 02 00 00       	mov    $0x2c3,%eax
-  400f88:	eb 34                	jmp    400fbe <phase_3+0x7b>
-  400f8a:	b8 00 01 00 00       	mov    $0x100,%eax
-  400f8f:	eb 2d                	jmp    400fbe <phase_3+0x7b>
-  400f91:	b8 85 01 00 00       	mov    $0x185,%eax
+  400f6a:	83 7c 24 08 07       	cmpl   $0x7,0x8(%rsp)                  # compare the first number with 7
+  400f6f:	77 3c                	ja     400fad <phase_3+0x6a>            # if the first number is greater than 7, jump to 400fad
+  400f71:	8b 44 24 08          	mov    0x8(%rsp),%eax                   # move the first number to eax
+  400f75:	ff 24 c5 70 24 40 00 	jmpq   *0x402470(,%rax,8)               # jump to the address in the jump table
+  400f7c:	b8 cf 00 00 00       	mov    $0xcf,%eax                       # 1st case
+  400f81:	eb 3b                	jmp    400fbe <phase_3+0x7b>            
+  400f83:	b8 c3 02 00 00       	mov    $0x2c3,%eax                      # 3rd case
+  400f88:	eb 34                	jmp    400fbe <phase_3+0x7b>            
+  400f8a:	b8 00 01 00 00       	mov    $0x100,%eax                      # 4th case
+  400f8f:	eb 2d                	jmp    400fbe <phase_3+0x7b>            #
+  400f91:	b8 85 01 00 00       	mov    $0x185,%eax                      # 5th case
   400f96:	eb 26                	jmp    400fbe <phase_3+0x7b>
-  400f98:	b8 ce 00 00 00       	mov    $0xce,%eax
+  400f98:	b8 ce 00 00 00       	mov    $0xce,%eax                       # 6th case
   400f9d:	eb 1f                	jmp    400fbe <phase_3+0x7b>
-  400f9f:	b8 aa 02 00 00       	mov    $0x2aa,%eax
+  400f9f:	b8 aa 02 00 00       	mov    $0x2aa,%eax                      # 7th case
   400fa4:	eb 18                	jmp    400fbe <phase_3+0x7b>
-  400fa6:	b8 47 01 00 00       	mov    $0x147,%eax
+  400fa6:	b8 47 01 00 00       	mov    $0x147,%eax                      # 8th case
   400fab:	eb 11                	jmp    400fbe <phase_3+0x7b>
   400fad:	e8 88 04 00 00       	callq  40143a <explode_bomb>
   400fb2:	b8 00 00 00 00       	mov    $0x0,%eax
   400fb7:	eb 05                	jmp    400fbe <phase_3+0x7b>
-  400fb9:	b8 37 01 00 00       	mov    $0x137,%eax
+  400fb9:	b8 37 01 00 00       	mov    $0x137,%eax                      # 2nd case
   400fbe:	3b 44 24 0c          	cmp    0xc(%rsp),%eax
   400fc2:	74 05                	je     400fc9 <phase_3+0x86>
   400fc4:	e8 71 04 00 00       	callq  40143a <explode_bomb>
@@ -446,17 +448,18 @@ Disassembly of section .text:
   40100c:	48 83 ec 18          	sub    $0x18,%rsp
   401010:	48 8d 4c 24 0c       	lea    0xc(%rsp),%rcx
   401015:	48 8d 54 24 08       	lea    0x8(%rsp),%rdx
-  40101a:	be cf 25 40 00       	mov    $0x4025cf,%esi
-  40101f:	b8 00 00 00 00       	mov    $0x0,%eax
+  40101a:	be cf 25 40 00       	mov    $0x4025cf,%esi                 # 0x4025cf = "%d %d"
+  40101f:	b8 00 00 00 00       	mov    $0x0,%eax                      # set eax to 0
   401024:	e8 c7 fb ff ff       	callq  400bf0 <__isoc99_sscanf@plt>
-  401029:	83 f8 02             	cmp    $0x2,%eax
+  401029:	83 f8 02             	cmp    $0x2,%eax                      # compare the return value with 2     eax must be 2   which is the return value of sscanf
   40102c:	75 07                	jne    401035 <phase_4+0x29>
-  40102e:	83 7c 24 08 0e       	cmpl   $0xe,0x8(%rsp)
-  401033:	76 05                	jbe    40103a <phase_4+0x2e>
+
+  40102e:	83 7c 24 08 0e       	cmpl   $0xe,0x8(%rsp)               
+  401033:	76 05                	jbe    40103a <phase_4+0x2e>            # Jump if 0xe(14)   below or equal   to 0x8(%rsp)
   401035:	e8 00 04 00 00       	callq  40143a <explode_bomb>
   40103a:	ba 0e 00 00 00       	mov    $0xe,%edx
   40103f:	be 00 00 00 00       	mov    $0x0,%esi
-  401044:	8b 7c 24 08          	mov    0x8(%rsp),%edi
+  401044:	8b 7c 24 08          	mov    0x8(%rsp),%edi                   # move the first number to edi
   401048:	e8 81 ff ff ff       	callq  400fce <func4>
   40104d:	85 c0                	test   %eax,%eax
   40104f:	75 07                	jne    401058 <phase_4+0x4c>
